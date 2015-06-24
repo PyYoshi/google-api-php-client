692c83feb64acec7dfb0d7547d329ea6d0b9dcd9
PyYoshi <myoshi321go@gmail.com>
2015-06-25 01:21:43 +0900

    test: show php version

--- run_test.sh
+++ run_test.sh
@@ -3,5 +3,7 @@
 export MEMCACHE_HOST=127.0.0.1
 export MEMCACHE_PORT=11211
 cd /tmp/google-api-php-client-test
+$(which php) --version
 $(which php) ./vendor/bin/phpunit -v
+$(which hhvm) --version
 $(which hhvm) ./vendor/bin/phpunit -v
